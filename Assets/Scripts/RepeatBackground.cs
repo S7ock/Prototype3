c787ce73fdfc18b01a256aa311afca40b6b17f9a
S7ock <161318992+S7ock@users.noreply.github.com>
2024-06-23 22:59:30 +0100

    Redefini a posição do fundo

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if (transform.position.x < startPos.x - 50) 
+        { 
+            transform.position = startPos; 
+        }  
     }
 }

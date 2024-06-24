82e1c9adfb2d38d2537b6018756a3e86dc75fe90
S7ock <161318992+S7ock@users.noreply.github.com>
2024-06-24 03:03:17 +0100

    Configurei uma animação de salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAmin;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAmin = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -25,6 +27,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAmin.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
